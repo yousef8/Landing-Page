70a912e8d2eb0b6b5f393f3a45054668efc5bee9
Yousef Elsayed <yousef4511@outlook.com>
2021-11-12 03:40:15 +0200

    Added Second Feature of Marking an Active Section

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-
+// First Feature
+// Building the NavBar
 function navBar() {
     let sections = document.querySelectorAll('section');
     let count = sections.length;
@@ -15,5 +16,25 @@ function navBar() {
     }
     document.querySelector('nav').style.display = 'block';
 }
-
 navBar();
+
+// Second Feature
+// Marking an Active Section
+let options = {
+    root: null, 
+    rootMargin: '0px', 
+    threshold: 0.5
+};
+
+let observer = new IntersectionObserver(function (entries, observer) {
+    entries.forEach(function(entry, index, entries) {
+        if (entry.isIntersecting === true) {
+            entry.target.className = 'activ-sec';
+        } else {
+            entry.target.classList.remove('activ-sec')
+        }
+    });
+}, options);
+
+let targets = document.querySelectorAll('section');
+targets.forEach((target) => {observer.observe(target)});
